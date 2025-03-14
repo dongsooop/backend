33d47341cfc24044e1b1b99e9f908febd62952bc
rdyjun <rdyjun00@gmail.com>
2025-03-14 14:46:40 +0900

    init: project init

---
+++ src/main/java/com/dongsoop/dongsoop/DongsoopApplication.java
@@ -0,0 +1,13 @@
+package com.dongsoop.dongsoop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DongsoopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DongsoopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dongsoop/dongsoop/DongsoopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongsoop.dongsoop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DongsoopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
