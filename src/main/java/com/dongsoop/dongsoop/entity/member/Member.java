2309f1e22ca586529ad633764255620dcc155dc3
alpin87 <qortmdals94@naver.com>
2025-03-20 10:57:25 +0900

    feat: add Member Entity

---
+++ src/main/java/com/dongsoop/dongsoop/entity/member/Member.java
@@ -0,0 +1,38 @@
+package com.dongsoop.dongsoop.entity.member;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "members")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(name = "student_number", nullable = false)
+    private String studentNumber;
+
+    @Column(nullable = false)
+    private String department;
+
+    @Builder
+    public Member(String email, String nickname, String studentNumber, String department) {
+        this.email = email;
+        this.nickname = nickname;
+        this.studentNumber = studentNumber;
+        this.department = department;
+    }
+}
