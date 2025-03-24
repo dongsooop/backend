b9e8ae2bbad9000c6240c17772885c6c3478c258
alpin87 <qortmdals94@naver.com>
2025-03-24 16:48:59 +0900

    refactor: interface Dto to entity setting

--- src/main/java/com/dongsoop/dongsoop/member/service/MemberDetailsService.java
+++ src/main/java/com/dongsoop/dongsoop/member/service/MemberDetailsService.java
@@ -1,11 +1,21 @@
 package com.dongsoop.dongsoop.member.service;
 
 import com.dongsoop.dongsoop.exception.domain.member.MemberNotFoundException;
+import com.dongsoop.dongsoop.member.dto.MemberDetailsDto;
 import com.dongsoop.dongsoop.member.entity.Member;
 import com.dongsoop.dongsoop.member.entity.MemberDetails;
+import com.dongsoop.dongsoop.member.entity.Role;
 import com.dongsoop.dongsoop.member.repository.MemberRepository;
+
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
+
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -18,12 +28,17 @@ public class MemberDetailsService implements UserDetailsService {
     private final MemberRepository memberRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Optional<Member> optionalMember = memberRepository.findByEmail(username);
-        optionalMember.orElseThrow(() -> new MemberNotFoundException());
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
 
-        Member member = optionalMember.get();
-        return new MemberDetails(member);
+        Member member = memberRepository.findByEmail(email)
+                .orElseThrow(MemberNotFoundException::new);
+
+        Role role = member.getRole();
+
+        List<SimpleGrantedAuthority> authorities = Stream.of(role.toString())
+                .map(SimpleGrantedAuthority::new)
+                .toList();
+
+        return new User(email, member.getPassword(), authorities);
     }
-
 }
