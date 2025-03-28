e3ad0fe35679f5110e04a7c52f64a10b12732d44
rdyjun <rdyjun00@gmail.com>
2025-03-28 17:27:41 +0900

    feat(notice): add Notice Entity

---
+++ src/main/java/com/dongsoop/dongsoop/notice/entity/Notice.java
@@ -0,0 +1,26 @@
+package com.dongsoop.dongsoop.notice.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Notice {
+
+    @Id
+    @Getter
+    private Long id;
+
+    private String writer;
+
+    private String title;
+
+    private String link;
+
+    private LocalDate createdAt;
+}
