8c9899dfaa0d0b410542dc930ad10c3c8c5a8663
rdyjun <rdyjun00@gmail.com>
2025-03-31 14:57:13 +0900

    refactor(notice): change notice repository's key Long to NoticeKey

--- src/main/java/com/dongsoop/dongsoop/notice/repository/NoticeRepository.java
+++ src/main/java/com/dongsoop/dongsoop/notice/repository/NoticeRepository.java
@@ -2,11 +2,12 @@ package com.dongsoop.dongsoop.notice.repository;
 
 import com.dongsoop.dongsoop.notice.dto.NoticeMaxIdByType;
 import com.dongsoop.dongsoop.notice.entity.Notice;
+import com.dongsoop.dongsoop.notice.entity.Notice.NoticeKey;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-public interface NoticeRepository extends JpaRepository<Notice, Long> {
+public interface NoticeRepository extends JpaRepository<Notice, NoticeKey> {
 
     @Query("SELECT n.id.department, MAX(n.id.noticeDetails.id) FROM Notice n GROUP BY n.id.department")
     List<NoticeMaxIdByType> findMaxIdGroupByType();
