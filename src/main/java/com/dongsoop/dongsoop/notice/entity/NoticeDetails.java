020212c292e0fb017e302adc3a50acc3873e9b12
rdyjun <rdyjun00@gmail.com>
2025-03-31 13:53:25 +0900

    refactor(notice): add constraint at notice details entity

--- src/main/java/com/dongsoop/dongsoop/notice/entity/NoticeDetails.java
+++ src/main/java/com/dongsoop/dongsoop/notice/entity/NoticeDetails.java
@@ -1,5 +1,6 @@
 package com.dongsoop.dongsoop.notice.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import java.time.LocalDate;
@@ -16,10 +17,13 @@ public class NoticeDetails {
     @Getter
     private Long id;
 
+    @Column(length = 20)
     private String writer;
 
+    @Column(length = 30)
     private String title;
 
+    @Column(length = 50)
     private String link;
 
     private LocalDate createdAt;
