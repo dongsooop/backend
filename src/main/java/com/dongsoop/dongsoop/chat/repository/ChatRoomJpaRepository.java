bc8318d9ad34e38c91d7df8af036db9b4156a2c9
alpin87 <qortmdals94@naver.com>
2025-04-04 18:31:02 +0900

    refactor: delete unused methods

--- src/main/java/com/dongsoop/dongsoop/chat/repository/ChatRoomJpaRepository.java
+++ src/main/java/com/dongsoop/dongsoop/chat/repository/ChatRoomJpaRepository.java
@@ -2,10 +2,6 @@ package com.dongsoop.dongsoop.chat.repository;
 
 import com.dongsoop.dongsoop.chat.entity.ChatRoomEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-import java.util.Optional;
 
 public interface ChatRoomJpaRepository extends JpaRepository<ChatRoomEntity, String> {
-    List<ChatRoomEntity> findByParticipantsContaining(String userId);
-    Optional<ChatRoomEntity> findByParticipantsContainingAndIsGroupChat(String userId, boolean isGroupChat);
 }
