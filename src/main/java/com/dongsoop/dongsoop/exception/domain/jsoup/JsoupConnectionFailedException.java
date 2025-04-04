a2a425677c4d398387f0476f11baad50cc820d64
rdyjun <rdyjun00@gmail.com>
2025-04-04 20:27:49 +0900

    refactor(jsoup): change JsoupConnectionFailedException interface to RuntimeException
    JsoupConnectionFailedException dosen't need http status

--- src/main/java/com/dongsoop/dongsoop/exception/domain/jsoup/JsoupConnectionFailedException.java
+++ src/main/java/com/dongsoop/dongsoop/exception/domain/jsoup/JsoupConnectionFailedException.java
@@ -1,12 +1,9 @@
 package com.dongsoop.dongsoop.exception.domain.jsoup;
 
-import com.dongsoop.dongsoop.exception.CustomException;
-import org.springframework.http.HttpStatus;
-
-public class JsoupConnectionFailedException extends CustomException {
+public class JsoupConnectionFailedException extends RuntimeException {
 
     public JsoupConnectionFailedException() {
-        super("Jsoup 커넥션 연결에 실패하였습니다", HttpStatus.INTERNAL_SERVER_ERROR);
+        super("Jsoup 커넥션 연결에 실패하였습니다");
     }
 
 }
