65b6a88caa1fc6344defe93ee5e45da4193edf18
alpin87 <qortmdals94@naver.com>
2025-04-04 18:53:26 +0900

    feat: create custom exception handling for expelled users

---
+++ src/main/java/com/dongsoop/dongsoop/exception/domain/websocket/UnauthorizedManagerActionException.java
@@ -0,0 +1,10 @@
+package com.dongsoop.dongsoop.exception.domain.websocket;
+
+import com.dongsoop.dongsoop.exception.CustomException;
+import org.springframework.http.HttpStatus;
+
+public class UnauthorizedManagerActionException extends CustomException {
+    public UnauthorizedManagerActionException() {
+        super("매니저만 이 작업을 수행할 수 있습니다.", HttpStatus.FORBIDDEN);
+    }
+}
