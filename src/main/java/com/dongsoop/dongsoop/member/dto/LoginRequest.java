2af914bba67da2d2b207536550b9762388dcdb4b
rdyjun <rdyjun00@gmail.com>
2025-04-07 14:55:51 +0900

    refactor(member): validate login request parameter

--- src/main/java/com/dongsoop/dongsoop/member/dto/LoginRequest.java
+++ src/main/java/com/dongsoop/dongsoop/member/dto/LoginRequest.java
@@ -1,5 +1,8 @@
 package com.dongsoop.dongsoop.member.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -7,7 +10,15 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 @Getter
 @NoArgsConstructor
 public class LoginRequest {
+    @Pattern(regexp = "^[a-zA-Z0-9]+@dongyang.ac.kr$", message = "email 형식이 올바르지 않습니다.")
+    @NotBlank
+    @NotEmpty
     private String email;
+
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[~!@#$%^&*()_\\-+=\\[\\]{}|\\\\;:'\"<>,.?/]).{8,20}$",
+            message = "비밀번호 형식이 올바르지 않습니다.")
+    @NotBlank
+    @NotEmpty
     private String password;
 
     public UsernamePasswordAuthenticationToken toAuthenticationToken() {
