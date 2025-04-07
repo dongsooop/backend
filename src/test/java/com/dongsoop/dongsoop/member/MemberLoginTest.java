40b5ec624b374e5fd91425819d676e69ad79a574
rdyjun <rdyjun00@gmail.com>
2025-04-07 14:57:26 +0900

    test(member): add test for exception by request password less than 8 length

---
+++ src/test/java/com/dongsoop/dongsoop/member/MemberLoginTest.java
@@ -0,0 +1,31 @@
+package com.dongsoop.dongsoop.member;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class MemberLoginTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    @DisplayName("비밀번호가 8자 미만일 경우 예외 발생")
+    void throws_an_exception_if_the_password_length_less_than_8() throws Exception {
+        String password = "abc!@12"; // 8자 미만
+
+        mockMvc.perform(post("/member/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{ \"email\": \"a@dongyang.ac.kr\", \"password\": \"" + password + "\" }")
+        ).andExpect(status().isBadRequest());
+    }
+}
