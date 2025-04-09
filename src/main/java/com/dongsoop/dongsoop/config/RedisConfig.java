72014852d6be2151357ca4584522c8e5a33f6c5c
alpin87 <qortmdals94@naver.com>
2025-04-09 16:10:04 +0900

    chore: Update Redis configuration for improved serialization handling

--- src/main/java/com/dongsoop/dongsoop/config/RedisConfig.java
+++ src/main/java/com/dongsoop/dongsoop/config/RedisConfig.java
@@ -52,9 +52,11 @@ public class RedisConfig {
         objectMapper.registerModule(new JavaTimeModule());
 
         PolymorphicTypeValidator typeValidator = BasicPolymorphicTypeValidator.builder()
-                .allowIfSubType("com.dongsoop.dongsoop.chat.entity.BaseEntity.class")
-                .allowIfSubType("java.util.Collection")
-                .allowIfSubType("java.time.temporal.Temporal")
+                .allowIfSubType("com.dongsoop.dongsoop.chat.entity.ChatRoom")
+                .allowIfSubType("com.dongsoop.dongsoop.chat.entity.ChatMessage")
+                .allowIfSubType("java.util.HashSet")
+                .allowIfSubType("java.util.ArrayList")
+                .allowIfSubType("java.time.LocalDateTime")
                 .build();
 
         objectMapper.activateDefaultTyping(
