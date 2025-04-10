24a2275701b069e34c48b994612c1cf20afd7185
rdyjun <rdyjun00@gmail.com>
2025-04-10 20:23:37 +0900

    refactor(LoginRequest): remove NotEmpty because duplicate with NotBlank

--- src/main/java/com/dongsoop/dongsoop/member/dto/LoginRequest.java
+++ src/main/java/com/dongsoop/dongsoop/member/dto/LoginRequest.java
@@ -1,7 +1,6 @@
 package com.dongsoop.dongsoop.member.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,13 +9,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class LoginRequest {
     @Pattern(regexp = "^[a-zA-Z0-9]+@dongyang.ac.kr$", message = "email 형식이 올바르지 않습니다.")
-    @NotBlank
-    @NotEmpty
+    @NotBlank(message = "로그인 시 이메일은 필수입니다.")
     private String email;
 
     @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[~!@#$%^&*()_\\-+=\\[\\]{}|\\\\;:'\"<>,.?/]).{8,20}$",
             message = "비밀번호 형식이 올바르지 않습니다.")
-    @NotBlank
-    @NotEmpty
+    @NotBlank(message = "로그인 시 비밀번호 입력은 필수입니다.")
     private String password;
 }
