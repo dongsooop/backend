5dcd323be84df6dc94cc2a7f95d411ed8f781cc2
rdyjun <rdyjun00@gmail.com>
2025-04-11 12:55:08 +0900

    refactor(Member): add createdAt field

--- src/main/java/com/dongsoop/dongsoop/member/entity/Member.java
+++ src/main/java/com/dongsoop/dongsoop/member/entity/Member.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -38,5 +39,8 @@ public class Member {
 
     @Column(name = "department", length = 20)
     private String department;
+
+    @Column(name = "createdAt", nullable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
 
 }
