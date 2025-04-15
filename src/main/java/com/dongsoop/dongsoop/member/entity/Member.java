495b37dfd9f395e77ee8a55422504974f0fa9e74
rdyjun <rdyjun00@gmail.com>
2025-04-15 11:32:09 +0900

    fix(Member): initialize Member created at field to use pre persist

--- src/main/java/com/dongsoop/dongsoop/member/entity/Member.java
+++ src/main/java/com/dongsoop/dongsoop/member/entity/Member.java
@@ -9,6 +9,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
@@ -46,6 +47,12 @@ public class Member {
     private Department department;
 
     @Column(name = "created_at", nullable = false)
-    private LocalDateTime createdAt = LocalDateTime.now();
+    private LocalDateTime createdAt;
 
+    @PrePersist
+    public void onPrePersist() {
+        if (this.createdAt == null) {
+            this.createdAt = LocalDateTime.now();
+        }
+    }
 }
