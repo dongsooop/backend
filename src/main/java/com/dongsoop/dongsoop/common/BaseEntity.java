cf821b6074b14ab9ba3cdb1008fb67e6fb1b7d37
rdyjun <rdyjun00@gmail.com>
2025-05-02 11:16:21 +0900

    refactor(BaseEntity): add SuperBuilder & NoArgsCOnstructor lombok annotation

--- src/main/java/com/dongsoop/dongsoop/common/BaseEntity.java
+++ src/main/java/com/dongsoop/dongsoop/common/BaseEntity.java
@@ -2,8 +2,12 @@ package com.dongsoop.dongsoop.common;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 @MappedSuperclass
+@SuperBuilder
+@NoArgsConstructor
 public abstract class BaseEntity {
 
     @Column(name = "is_deleted", nullable = false, columnDefinition = "boolean default false")
