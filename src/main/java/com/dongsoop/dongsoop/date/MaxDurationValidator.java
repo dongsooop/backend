fdb771ed679d0cbd0f62937f2a4757808b5408e6
rdyjun <rdyjun00@gmail.com>
2025-05-09 10:44:35 +0900

    refactor(duration): extends DurationValidator

--- src/main/java/com/dongsoop/dongsoop/date/MaxDurationValidator.java
+++ src/main/java/com/dongsoop/dongsoop/date/MaxDurationValidator.java
@@ -1,17 +1,12 @@
 package com.dongsoop.dongsoop.date;
 
-import jakarta.validation.ConstraintValidator;
-import jakarta.validation.ConstraintValidatorContext;
-import java.lang.reflect.Field;
 import java.time.LocalDateTime;
 
-public class MaxDurationValidator implements ConstraintValidator<MaxDuration, Object> {
+public class MaxDurationValidator extends DurationValidator<MaxDuration> {
 
-    private Long value;
-
-    private String startFieldName;
-
-    private String endFieldName;
+    public MaxDurationValidator() {
+        super();
+    }
 
     @Override
     public void initialize(MaxDuration constraintAnnotation) {
@@ -21,23 +16,8 @@ public class MaxDurationValidator implements ConstraintValidator<MaxDuration, Ob
     }
 
     @Override
-    public boolean isValid(Object object, ConstraintValidatorContext constraintValidatorContext) {
-        try {
-            Field startField = object.getClass()
-                    .getDeclaredField(startFieldName);
-            Field endField = object.getClass()
-                    .getDeclaredField(endFieldName);
-
-            startField.setAccessible(true);
-            endField.setAccessible(true);
-
-            LocalDateTime startAt = (LocalDateTime) startField.get(object);
-            LocalDateTime endAt = (LocalDateTime) endField.get(object);
-
-            LocalDateTime maxTime = startAt.plusSeconds(this.value);
-            return endAt.isBefore(maxTime) || endAt.equals(maxTime);
-        } catch (Exception e) {
-            return false;
-        }
+    public boolean validateDuration() {
+        LocalDateTime maxTime = startDateTime.plusSeconds(this.value);
+        return endDateTime.isBefore(maxTime) || endDateTime.equals(maxTime);
     }
 }
--- src/main/java/com/dongsoop/dongsoop/date/MinDurationValidator.java
+++ src/main/java/com/dongsoop/dongsoop/date/MinDurationValidator.java
@@ -1,17 +1,12 @@
 package com.dongsoop.dongsoop.date;
 
-import jakarta.validation.ConstraintValidator;
-import jakarta.validation.ConstraintValidatorContext;
-import java.lang.reflect.Field;
 import java.time.LocalDateTime;
 
-public class MinDurationValidator implements ConstraintValidator<MinDuration, Object> {
+public class MinDurationValidator extends DurationValidator<MinDuration> {
 
-    private Long value;
-
-    private String startFieldName;
-
-    private String endFieldName;
+    public MinDurationValidator() {
+        super();
+    }
 
     @Override
     public void initialize(MinDuration constraintAnnotation) {
@@ -21,27 +16,8 @@ public class MinDurationValidator implements ConstraintValidator<MinDuration, Ob
     }
 
     @Override
-    public boolean isValid(Object object, ConstraintValidatorContext constraintValidatorContext) {
-        try {
-            Field startField = object.getClass()
-                    .getDeclaredField(startFieldName);
-            Field endField = object.getClass()
-                    .getDeclaredField(endFieldName);
-
-            startField.setAccessible(true);
-            endField.setAccessible(true);
-
-            LocalDateTime startAt = (LocalDateTime) startField.get(object);
-            LocalDateTime endAt = (LocalDateTime) endField.get(object);
-
-            if (startAt == null || endAt == null) {
-                return true; // null 검증은 @NotNull 에서 처리
-            }
-
-            LocalDateTime minTime = endAt.minusSeconds(this.value);
-            return startAt.isBefore(minTime) || startAt.equals(minTime);
-        } catch (Exception e) {
-            return false;
-        }
+    public boolean validateDuration() {
+        LocalDateTime minTime = endDateTime.minusSeconds(this.value);
+        return startDateTime.isBefore(minTime) || startDateTime.equals(minTime);
     }
 }
