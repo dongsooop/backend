423636bef722ca97b7dafd5b8eaa2c9ed2a6eee8
alpin87 <qortmdals94@naver.com>
2025-05-21 09:56:14 +0900

    rename: change TokenIssueResponse to IssuedToken

--- src/main/java/com/dongsoop/dongsoop/jwt/dto/IssuedToken.java
+++ src/main/java/com/dongsoop/dongsoop/jwt/dto/IssuedToken.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class TokenIssueResponse {
+public class IssuedToken {
     private String accessToken;
     private String refreshToken;
 }
