318e1cc3e0323f789683933eae2afe6d5d54208c
rdyjun <rdyjun00@gmail.com>
2025-05-23 20:27:33 +0900

    refactor(StudyBoardDepartmentRepository): change key's class

--- src/main/java/com/dongsoop/dongsoop/study/repository/StudyBoardDepartmentRepository.java
+++ src/main/java/com/dongsoop/dongsoop/study/repository/StudyBoardDepartmentRepository.java
@@ -1,7 +1,8 @@
 package com.dongsoop.dongsoop.study.repository;
 
 import com.dongsoop.dongsoop.study.entity.StudyBoardDepartment;
+import com.dongsoop.dongsoop.study.entity.StudyBoardDepartment.StudyBoardDepartmentId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StudyBoardDepartmentRepository extends JpaRepository<StudyBoardDepartment, Long> {
+public interface StudyBoardDepartmentRepository extends JpaRepository<StudyBoardDepartment, StudyBoardDepartmentId> {
 }
