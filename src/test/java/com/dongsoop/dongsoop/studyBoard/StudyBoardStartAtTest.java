225a0fa31c553c31df461191f5f7b83e988e1b75
rdyjun <rdyjun00@gmail.com>
2025-05-23 11:01:58 +0900

    test(study-board): test study board create

---
+++ src/test/java/com/dongsoop/dongsoop/studyBoard/StudyBoardStartAtTest.java
@@ -0,0 +1,130 @@
+package com.dongsoop.dongsoop.studyBoard;
+
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.dongsoop.dongsoop.department.entity.DepartmentType;
+import com.dongsoop.dongsoop.jwt.filter.JwtFilter;
+import com.dongsoop.dongsoop.study.controller.StudyBoardController;
+import com.dongsoop.dongsoop.study.dto.CreateStudyBoardRequest;
+import com.dongsoop.dongsoop.study.entity.StudyBoard;
+import com.dongsoop.dongsoop.study.service.StudyBoardServiceImpl;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+
+@WebMvcTest(controllers = StudyBoardController.class)
+@AutoConfigureMockMvc(addFilters = false)
+class StudyBoardStartAtTest {
+
+    private final JSONObject json = new JSONObject();
+
+    private final String REQUEST_URL = "/study-board";
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private StudyBoardServiceImpl studyBoardService;
+
+    @MockitoBean
+    private JwtFilter jwtFilter;
+
+    @BeforeEach
+    void setUp() throws JSONException {
+        json.put("title", "title");
+        json.put("tags", "tags");
+        json.put("content", "content");
+        json.put("departmentType", new DepartmentType[]{DepartmentType.DEPT_2001, DepartmentType.DEPT_3001});
+    }
+
+    @Test
+    @DisplayName("모집 게시판 생성 시 모집 시작일이 과거일 경우 예외를 던진다")
+    void startRecruitment_WithPast_ShouldThrowException() throws Exception {
+        // given
+        json.put("startAt", LocalDateTime.now());
+        json.put("endAt", LocalDateTime.now().minusHours(1));
+
+        // when
+        MockHttpServletRequestBuilder content = post(REQUEST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json.toString());
+
+        // then
+        MvcResult result = mockMvc.perform(content)
+                .andExpect(status().isBadRequest())
+                .andReturn();
+
+        Throwable throwable = result.getResolvedException();
+        assertNotNull(throwable);
+        assertInstanceOf(MethodArgumentNotValidException.class, throwable);
+    }
+
+    @Test
+    @DisplayName("모집 게시판 생성 시 모집 시작일이 오늘일 경우 성공 상태를 반환한다")
+    void startRecruitment_WithToday_ShouldResponseSuccess() throws Exception {
+        // given
+        StudyBoard studyBoard = StudyBoard.builder()
+                .id(1L)
+                .title("title")
+                .content("content")
+                .tags("tags")
+                .startAt(LocalDate.now().atStartOfDay())
+                .endAt(LocalDate.now().atStartOfDay())
+                .build();
+
+        when(studyBoardService.create(any(CreateStudyBoardRequest.class)))
+                .thenReturn(studyBoard);
+
+        json.put("startAt", LocalDate.now().atStartOfDay());
+        json.put("endAt", LocalDate.now().atStartOfDay().plusDays(1));
+
+        // when
+        MockHttpServletRequestBuilder httpContent = post(REQUEST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json.toString());
+
+        // then
+        mockMvc.perform(httpContent)
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    @DisplayName("모집 게시판 생성 시 모집 시작일이 null인 경우 예외를 던진다")
+    void startRecruitment_WithNull_ShouldThrowException() throws Exception {
+        // given
+        json.put("startAt", null);
+        json.put("endAt", null);
+
+        // when
+        MockHttpServletRequestBuilder content = post(REQUEST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json.toString());
+
+        // then
+        MvcResult result = mockMvc.perform(content)
+                .andExpect(status().isBadRequest())
+                .andReturn();
+
+        Throwable throwable = result.getResolvedException();
+        assertNotNull(throwable);
+        assertInstanceOf(MethodArgumentNotValidException.class, throwable);
+    }
+}
