0f6f6cdc7dabb4c1d5f66ffb5db6d10c39a8d5a4
alpin87 <qortmdals94@naver.com>
2025-05-29 11:04:01 +0900

    feat: Add manualCrawl method to service interface

--- src/main/java/com/dongsoop/dongsoop/meal/service/MealService.java
+++ src/main/java/com/dongsoop/dongsoop/meal/service/MealService.java
@@ -1,23 +1,7 @@
 package com.dongsoop.dongsoop.meal.service;
 
-import com.dongsoop.dongsoop.meal.dto.MealListDto;
 import com.dongsoop.dongsoop.meal.dto.MealWeeklyResponse;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-
-import java.time.LocalDate;
 
 public interface MealService {
-
-    Page<MealListDto> getWeeklyMeal(LocalDate startDate, LocalDate endDate, Pageable pageable);
-
-    Page<MealListDto> getCurrentWeekMeal(Pageable pageable);
-
-    MealWeeklyResponse getWeeklyMealResponse(LocalDate startDate, LocalDate endDate);
-
     MealWeeklyResponse getCurrentWeekMealResponse();
-
-    void manualCrawl();
-
-    void manualCleanup();
 }
