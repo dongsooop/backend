45e91ec980c92f7c60137fdf431fc5d0f0f1739a
alpin87 <qortmdals94@naver.com>
2025-05-31 00:19:42 +0900

    refactor: Delete Unnecessary Logs

--- src/main/java/com/dongsoop/dongsoop/meal/controller/MealController.java
+++ src/main/java/com/dongsoop/dongsoop/meal/controller/MealController.java
@@ -3,10 +3,10 @@ package com.dongsoop.dongsoop.meal.controller;
 import com.dongsoop.dongsoop.meal.dto.MealWeeklyResponse;
 import com.dongsoop.dongsoop.meal.service.MealService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.CacheControl;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +15,6 @@ import java.util.concurrent.TimeUnit;
 @RestController
 @RequestMapping("/meal")
 @RequiredArgsConstructor
-@Slf4j
 public class MealController {
 
     private static final CacheControl CACHE_CONTROL = CacheControl
@@ -26,11 +25,7 @@ public class MealController {
 
     @GetMapping("/current")
     public ResponseEntity<MealWeeklyResponse> getCurrentWeekMeal() {
-        log.info("현재 주 식단 조회 요청");
-
         MealWeeklyResponse result = mealService.getCurrentWeekMealResponse();
-
-        log.info("현재 주 식단 조회 결과 - {}일간 데이터", result.getDailyMeals().size());
 
         return ResponseEntity.ok()
                 .cacheControl(CACHE_CONTROL)
