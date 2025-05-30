88bfe82fca10f59f497cce2dbe5ec4afed816e13
rdyjun <rdyjun00@gmail.com>
2025-05-30 19:41:57 +0900

    refactor(NotScheduleOwnerException): change exception message

--- src/main/java/com/dongsoop/dongsoop/exception/domain/schedule/NotScheduleOwnerException.java
+++ src/main/java/com/dongsoop/dongsoop/exception/domain/schedule/NotScheduleOwnerException.java
@@ -6,6 +6,6 @@ import org.springframework.http.HttpStatus;
 public class NotScheduleOwnerException extends CustomException {
 
     public NotScheduleOwnerException(Long memberId, Long memberScheduleId) {
-        super("요청한 회원의 일정이 아닙니다. 회원 ID:" + memberId + ", 게시글 ID: " + memberScheduleId, HttpStatus.NOT_FOUND);
+        super("요청한 회원의 일정이 아닙니다. 회원 ID:" + memberId + ", 회원 일정 ID: " + memberScheduleId, HttpStatus.NOT_FOUND);
     }
 }
