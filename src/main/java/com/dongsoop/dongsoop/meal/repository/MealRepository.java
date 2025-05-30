c98bd3b65620c30abd5f7692efc9a97b5541a8f7
alpin87 <qortmdals94@naver.com>
2025-05-30 18:25:03 +0900

    refactor: maintain existing repository interface

--- src/main/java/com/dongsoop/dongsoop/meal/repository/MealRepository.java
+++ src/main/java/com/dongsoop/dongsoop/meal/repository/MealRepository.java
@@ -3,6 +3,15 @@ package com.dongsoop.dongsoop.meal.repository;
 import com.dongsoop.dongsoop.meal.entity.Meal;
 import com.dongsoop.dongsoop.meal.entity.Meal.MealKey;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
 
 public interface MealRepository extends JpaRepository<Meal, MealKey> {
+
+    @Modifying(clearAutomatically = true)
+    @Query("DELETE FROM Meal m WHERE m.id.mealDetails.mealDate BETWEEN :startDate AND :endDate")
+    int deleteByMealDateBetween(@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
 }
