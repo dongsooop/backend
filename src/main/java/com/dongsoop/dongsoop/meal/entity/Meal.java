d2a8aa8f8434f46dc67bf4658d089ce9202a3399
alpin87 <qortmdals94@naver.com>
2025-05-30 22:32:15 +0900

    refactor: consolidate MealDetails entity into Meal entity

--- src/main/java/com/dongsoop/dongsoop/meal/entity/Meal.java
+++ src/main/java/com/dongsoop/dongsoop/meal/entity/Meal.java
@@ -1,51 +1,73 @@
 package com.dongsoop.dongsoop.meal.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Objects;
 
 @Entity
-@Table(name = "meal")
+@Table(name = "meal",
+        indexes = {
+                @Index(name = "idx_meal_date_type", columnList = "meal_date, meal_type"),
+                @Index(name = "idx_meal_date", columnList = "meal_date")
+        }
+)
+@EntityListeners(AuditingEntityListener.class)
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-@NoArgsConstructor
+@Builder
 public class Meal {
 
-    @EmbeddedId
-    private MealKey id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    public Meal(MealDetails mealDetails) {
-        this.id = new MealKey(mealDetails);
+    @Column(name = "meal_date", nullable = false)
+    private LocalDate mealDate;
+
+    @Column(name = "day_of_week", nullable = false, length = 10)
+    private String dayOfWeek;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "meal_type", nullable = false, length = 20)
+    private MealType mealType;
+
+    @Column(name = "menu_items", columnDefinition = "TEXT")
+    private String menuItems;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Meal meal = (Meal) o;
+
+        if (id == null && meal.id == null) {
+            return Objects.equals(mealDate, meal.mealDate) &&
+                    Objects.equals(mealType, meal.mealType);
+        }
+
+        return Objects.equals(id, meal.id);
     }
 
-    @Embeddable
-    @AllArgsConstructor
-    @NoArgsConstructor
-    @Getter
-    public static class MealKey implements java.io.Serializable {
-
-        @ManyToOne(fetch = FetchType.LAZY)
-        @JoinColumn(name = "meal_details_id")
-        private MealDetails mealDetails;
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) {
-                return true;
-            }
-            if (o == null || getClass() != o.getClass()) {
-                return false;
-            }
-            MealKey that = (MealKey) o;
-            return Objects.equals(mealDetails, that.mealDetails);
+    @Override
+    public int hashCode() {
+        if (id == null) {
+            return Objects.hash(mealDate, mealType);
         }
 
-        @Override
-        public int hashCode() {
-            return Objects.hash(mealDetails);
-        }
+        return Objects.hash(id);
     }
 }
