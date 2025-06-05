bc8104acd23f9149154f53a81e2c22db906807d9
rdyjun <rdyjun00@gmail.com>
2025-06-05 16:27:18 +0900

    feat(project apply: throw exception when project recruitment board department is not same to volunteer department

--- src/main/java/com/dongsoop/dongsoop/recruitment/project/service/ProjectApplyServiceImpl.java
+++ src/main/java/com/dongsoop/dongsoop/recruitment/project/service/ProjectApplyServiceImpl.java
@@ -1,13 +1,19 @@
 package com.dongsoop.dongsoop.recruitment.project.service;
 
+import com.dongsoop.dongsoop.department.entity.Department;
+import com.dongsoop.dongsoop.department.entity.DepartmentType;
+import com.dongsoop.dongsoop.exception.domain.project.ProjectBoardDepartmentMismatchException;
+import com.dongsoop.dongsoop.exception.domain.project.ProjectBoardDepartmentNotAssignedException;
 import com.dongsoop.dongsoop.member.entity.Member;
 import com.dongsoop.dongsoop.member.service.MemberService;
 import com.dongsoop.dongsoop.recruitment.project.dto.ApplyProjectBoardRequest;
 import com.dongsoop.dongsoop.recruitment.project.entity.ProjectBoard;
 import com.dongsoop.dongsoop.recruitment.project.entity.ProjectBoardApply;
 import com.dongsoop.dongsoop.recruitment.project.entity.ProjectBoardApply.ProjectBoardApplyKey;
+import com.dongsoop.dongsoop.recruitment.project.entity.ProjectBoardDepartment;
 import com.dongsoop.dongsoop.recruitment.project.repository.ProjectApplyRepository;
 import com.dongsoop.dongsoop.recruitment.project.repository.ProjectBoardRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -23,10 +29,17 @@ public class ProjectApplyServiceImpl implements ProjectApplyService {
 
     public void apply(ApplyProjectBoardRequest request) {
         Member member = memberService.getMemberReferenceByContext();
-        ProjectBoard referenceById = projectBoardRepository.getReferenceById(request.boardId());
+        ProjectBoard projectBoard = projectBoardRepository.getReferenceById(request.boardId());
 
-        ProjectBoardApplyKey key = new ProjectBoardApplyKey(referenceById, member);
+        List<ProjectBoardDepartment> boardDepartmentList = projectBoardRepository.findByProjectBoardId(
+                request.boardId());
+        if (boardDepartmentList.isEmpty()) {
+            throw new ProjectBoardDepartmentNotAssignedException(request.boardId());
+        }
 
+        validateDepartment(boardDepartmentList, member);
+
+        ProjectBoardApplyKey key = new ProjectBoardApplyKey(projectBoard, member);
         ProjectBoardApply boardApply = ProjectBoardApply.builder()
                 .id(key)
                 .introduction(request.introduction())
@@ -34,5 +47,22 @@ public class ProjectApplyServiceImpl implements ProjectApplyService {
                 .build();
 
         projectApplyRepository.save(boardApply);
+    }
+
+    private void validateDepartment(List<ProjectBoardDepartment> boardDepartmentList, Member member) {
+        DepartmentType requesterDepartmentType = member.getDepartment().getId();
+
+        for (ProjectBoardDepartment boardDepartment : boardDepartmentList) {
+            if (boardDepartment.isSameDepartmentType(requesterDepartmentType)) {
+                return;
+            }
+        }
+
+        List<DepartmentType> boardDepartmentTypeList = boardDepartmentList.stream()
+                .map(ProjectBoardDepartment::getDepartment)
+                .map(Department::getId)
+                .toList();
+
+        throw new ProjectBoardDepartmentMismatchException(boardDepartmentTypeList, requesterDepartmentType);
     }
 }
