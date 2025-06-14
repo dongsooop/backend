675a61a18cf7702882eb32cdacce9baf29012cf8
rdyjun <rdyjun00@gmail.com>
2025-06-14 15:22:35 +0900

    refactor(marketplace-contact): add @RequestBody

--- src/main/java/com/dongsoop/dongsoop/marketplace/controller/MarketplaceContactController.java
+++ src/main/java/com/dongsoop/dongsoop/marketplace/controller/MarketplaceContactController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +21,7 @@ public class MarketplaceContactController {
 
     @PostMapping
     @Secured(RoleType.USER_ROLE)
-    public ResponseEntity<Void> contactMarketplace(ContactMarketplaceRequest request) {
+    public ResponseEntity<Void> contactMarketplace(@RequestBody ContactMarketplaceRequest request) {
         marketplaceContactService.contact(request);
 
         URI uri = URI.create("/marketplace-board/" + request.boardId());
