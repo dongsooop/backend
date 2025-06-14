fd0eeabc39a183504ff667523cd6060c7e17aec0
rdyjun <rdyjun00@gmail.com>
2025-06-14 15:29:04 +0900

    feat(market-contact): validate board not found

--- src/main/java/com/dongsoop/dongsoop/marketplace/service/MarketplaceContactServiceImpl.java
+++ src/main/java/com/dongsoop/dongsoop/marketplace/service/MarketplaceContactServiceImpl.java
@@ -1,8 +1,10 @@
 package com.dongsoop.dongsoop.marketplace.service;
 
-import com.dongsoop.dongsoop.exception.domain.marketplace.MarketplaceAlreadyAppliedException;
+import com.dongsoop.dongsoop.exception.domain.marketplace.MarketplaceAlreadyContactException;
+import com.dongsoop.dongsoop.exception.domain.marketplace.MarketplaceBoardNotFoundException;
 import com.dongsoop.dongsoop.marketplace.dto.ContactMarketplaceRequest;
 import com.dongsoop.dongsoop.marketplace.entity.MarketplaceContact;
+import com.dongsoop.dongsoop.marketplace.repository.MarketplaceBoardRepository;
 import com.dongsoop.dongsoop.marketplace.repository.MarketplaceContactRepository;
 import com.dongsoop.dongsoop.marketplace.repository.MarketplaceContactRepositoryCustom;
 import com.dongsoop.dongsoop.member.service.MemberService;
@@ -17,20 +19,37 @@ public class MarketplaceContactServiceImpl implements MarketplaceContactService 
 
     private final MarketplaceContactRepositoryCustom marketplaceContactRepositoryCustom;
 
+    private final MarketplaceBoardRepository marketplaceBoardRepository;
+
     private final MarketplaceContactMapper marketplaceContactMapper;
 
     private final MemberService memberService;
 
     public void contact(ContactMarketplaceRequest request) {
-        Long memberId = memberService.getMemberIdByAuthentication();
-        boolean isAlreadyApplied = marketplaceContactRepositoryCustom.existsByBoardIdAndMemberId(request.boardId(),
-                memberId);
+        Long boardId = request.boardId();
 
-        if (isAlreadyApplied) {
-            throw new MarketplaceAlreadyAppliedException(memberId, request.boardId());
-        }
+        validateBoardExists(boardId);
+
+        Long memberId = memberService.getMemberIdByAuthentication();
+        validateAlreadyContact(memberId, boardId);
 
         MarketplaceContact marketplaceContact = marketplaceContactMapper.toEntity(request);
         marketplaceContactRepository.save(marketplaceContact);
+    }
+
+    private void validateBoardExists(Long boardId) {
+        boolean isExistsBoard = marketplaceBoardRepository.existsById(boardId);
+        if (isExistsBoard) {
+            throw new MarketplaceBoardNotFoundException(boardId);
+        }
+    }
+
+    private void validateAlreadyContact(Long memberId, Long boardId) {
+        boolean isAlreadyApplied = marketplaceContactRepositoryCustom.existsByBoardIdAndMemberId(boardId,
+                memberId);
+
+        if (isAlreadyApplied) {
+            throw new MarketplaceAlreadyContactException(memberId, boardId);
+        }
     }
 }
