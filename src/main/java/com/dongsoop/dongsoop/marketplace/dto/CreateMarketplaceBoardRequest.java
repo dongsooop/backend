0952283846ca991f8e6c36505d1ac4bc26908a72
rdyjun <rdyjun00@gmail.com>
2025-06-14 17:34:15 +0900

    refactor(CreateMarketplaceBoardRequest): add validator annotation

--- src/main/java/com/dongsoop/dongsoop/marketplace/dto/CreateMarketplaceBoardRequest.java
+++ src/main/java/com/dongsoop/dongsoop/marketplace/dto/CreateMarketplaceBoardRequest.java
@@ -1,8 +1,19 @@
 package com.dongsoop.dongsoop.marketplace.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
 public record CreateMarketplaceBoardRequest(
+
+        @NotBlank
         String title,
+
+        @NotBlank
         String content,
+
+        @NotNull
+        @Positive
         Long price
 ) {
 }
