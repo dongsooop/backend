3bfed4f1163c6e445e87ca064333ba54b0eb3e2d
rdyjun <rdyjun00@gmail.com>
2025-06-23 19:36:14 +0900

    refactor(board-tags): add @BoardTag annotation

--- src/main/java/com/dongsoop/dongsoop/recruitment/study/dto/CreateStudyBoardRequest.java
+++ src/main/java/com/dongsoop/dongsoop/recruitment/study/dto/CreateStudyBoardRequest.java
@@ -6,10 +6,9 @@ import com.dongsoop.dongsoop.date.MaxDuration;
 import com.dongsoop.dongsoop.date.MinDuration;
 import com.dongsoop.dongsoop.date.TodayOrFuture;
 import com.dongsoop.dongsoop.department.entity.DepartmentType;
-import jakarta.annotation.Nullable;
+import com.dongsoop.dongsoop.recruitment.validation.annotation.BoardTag;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -25,9 +24,7 @@ public record CreateStudyBoardRequest(
         @NotBlank
         String content,
 
-        @Nullable
-        @Pattern(regexp = "^[a-zA-Z0-9가-힣]+$", message = "태그는 공백을 포함하지 않는 영문, 숫자, 한글만 허용됩니다.")
-        @Size(max = 100)
+        @BoardTag
         String tags,
 
         @NotNull
