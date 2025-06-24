a14a09b25dff27c208fe49e636525608a9600431
rdyjun <rdyjun00@gmail.com>
2025-06-24 11:20:57 +0900

    fix(BoardTag): add validate annotations field

--- src/main/java/com/dongsoop/dongsoop/recruitment/validation/annotation/BoardTag.java
+++ src/main/java/com/dongsoop/dongsoop/recruitment/validation/annotation/BoardTag.java
@@ -1,6 +1,7 @@
 package com.dongsoop.dongsoop.recruitment.validation.annotation;
 
 import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import java.lang.annotation.ElementType;
@@ -14,4 +15,10 @@ import java.lang.annotation.Target;
 @Size(max = 100)
 @Constraint(validatedBy = {})
 public @interface BoardTag {
+
+    String message() default "태그는 한글, 영문, 숫자만 포함할 수 있으며 최대 100자입니다";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
 }
