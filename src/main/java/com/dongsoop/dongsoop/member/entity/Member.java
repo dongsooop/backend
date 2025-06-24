77fd1af2715a0bbdf1900e48367d9444cf616be0
rdyjun <rdyjun00@gmail.com>
2025-06-24 09:56:03 +0900

    refactor(Member): remove createdAt field
    already BaseEntity has createdAt

--- src/main/java/com/dongsoop/dongsoop/member/entity/Member.java
+++ src/main/java/com/dongsoop/dongsoop/member/entity/Member.java
@@ -10,9 +10,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
-import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -48,14 +46,5 @@ public class Member extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "department_id")
     private Department department;
-
-    @Column(name = "created_at", nullable = false)
-    private LocalDateTime createdAt;
-
-    @PrePersist
-    public void onPrePersist() {
-        if (this.createdAt == null) {
-            this.createdAt = LocalDateTime.now();
-        }
-    }
+    
 }
