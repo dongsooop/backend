bb1265ca1bdf58afe6b9995ac019f1278cf82dfe
alpin87 <qortmdals94@naver.com>
2025-06-26 17:26:57 +0900

    style: Corrected typo

--- src/main/java/com/dongsoop/dongsoop/exception/domain/report/ReportNotFoundException.java
+++ src/main/java/com/dongsoop/dongsoop/exception/domain/report/ReportNotFoundException.java
@@ -5,6 +5,6 @@ import org.springframework.http.HttpStatus;
 
 public class ReportNotFoundException extends CustomException {
     public ReportNotFoundException(Long reportId) {
-        super("존재하지 않는 신고합니다. ID :" + reportId, HttpStatus.NOT_FOUND);
+        super("존재하지 않는 신고입니다. ID :" + reportId, HttpStatus.NOT_FOUND);
     }
 }
