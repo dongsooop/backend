68f3fdc2fb1d1ce9de2089e58532c7a1db0e6e2c
alpin87 <qortmdals94@naver.com>
2025-06-30 17:12:07 +0900

    refactor: use repository instances instead of method references in ContentDeletionHandler

--- src/main/java/com/dongsoop/dongsoop/report/handler/ContentDeletionHandler.java
+++ src/main/java/com/dongsoop/dongsoop/report/handler/ContentDeletionHandler.java
@@ -7,11 +7,10 @@ import com.dongsoop.dongsoop.recruitment.tutoring.repository.TutoringBoardReposi
 import com.dongsoop.dongsoop.report.entity.Report;
 import com.dongsoop.dongsoop.report.entity.ReportType;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
-import java.util.Optional;
-import java.util.function.Consumer;
 
 @Component
 @RequiredArgsConstructor
@@ -23,16 +22,19 @@ public class ContentDeletionHandler {
     private final TutoringBoardRepository tutoringBoardRepository;
 
     public void deleteContent(Report report) {
-        Optional.ofNullable(getContentDeleters().get(report.getReportType()))
-                .ifPresent(deleter -> deleter.accept(report.getTargetId()));
+        JpaRepository<?, Long> repository = getContentDeleters().get(report.getReportType());
+
+        if (repository != null) {
+            repository.deleteById(report.getTargetId());
+        }
     }
 
-    private Map<ReportType, Consumer<Long>> getContentDeleters() {
+    private Map<ReportType, JpaRepository<?, Long>> getContentDeleters() {
         return Map.of(
-                ReportType.PROJECT_BOARD, projectBoardRepository::deleteById,
-                ReportType.STUDY_BOARD, studyBoardRepository::deleteById,
-                ReportType.MARKETPLACE_BOARD, marketplaceBoardRepository::deleteById,
-                ReportType.TUTORING_BOARD, tutoringBoardRepository::deleteById
+                ReportType.PROJECT_BOARD, projectBoardRepository,
+                ReportType.STUDY_BOARD, studyBoardRepository,
+                ReportType.MARKETPLACE_BOARD, marketplaceBoardRepository,
+                ReportType.TUTORING_BOARD, tutoringBoardRepository
         );
     }
 }
