e57ce7569621ccfd9b984c09352fc7a2ae45e8ca
alpin87 <qortmdals94@naver.com>
2025-06-30 17:11:35 +0900

    refactor: update ReportService interface to return List instead of Page

--- src/main/java/com/dongsoop/dongsoop/report/service/ReportService.java
+++ src/main/java/com/dongsoop/dongsoop/report/service/ReportService.java
@@ -4,14 +4,15 @@ import com.dongsoop.dongsoop.report.dto.CreateReportRequest;
 import com.dongsoop.dongsoop.report.dto.ProcessSanctionRequest;
 import com.dongsoop.dongsoop.report.dto.ReportResponse;
 import com.dongsoop.dongsoop.report.entity.ReportFilterType;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 public interface ReportService {
 
     void createReport(CreateReportRequest request);
 
     void processSanction(ProcessSanctionRequest request);
-    
-    Page<ReportResponse> getReports(ReportFilterType filterType, Pageable pageable);
+
+    List<ReportResponse> getReports(ReportFilterType filterType, Pageable pageable);
 }
