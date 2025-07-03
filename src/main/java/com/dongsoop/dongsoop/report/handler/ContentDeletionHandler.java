9710a6af50d51bf0568ac8c7da9491c5f9a08777
alpin87 <qortmdals94@naver.com>
2025-07-03 11:21:04 +0900

    refactor: optimize content deleters map creation with static final field

--- src/main/java/com/dongsoop/dongsoop/report/handler/ContentDeletionHandler.java
+++ src/main/java/com/dongsoop/dongsoop/report/handler/ContentDeletionHandler.java
@@ -6,35 +6,33 @@ import com.dongsoop.dongsoop.recruitment.study.repository.StudyBoardRepository;
 import com.dongsoop.dongsoop.recruitment.tutoring.repository.TutoringBoardRepository;
 import com.dongsoop.dongsoop.report.entity.Report;
 import com.dongsoop.dongsoop.report.entity.ReportType;
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
 
 @Component
-@RequiredArgsConstructor
 public class ContentDeletionHandler {
 
-    private final ProjectBoardRepository projectBoardRepository;
-    private final StudyBoardRepository studyBoardRepository;
-    private final MarketplaceBoardRepository marketplaceBoardRepository;
-    private final TutoringBoardRepository tutoringBoardRepository;
+    private final Map<ReportType, JpaRepository<?, Long>> contentDeleters;
 
-    public void deleteContent(Report report) {
-        JpaRepository<?, Long> repository = getContentDeleters().get(report.getReportType());
-
-        if (repository != null) {
-            repository.deleteById(report.getTargetId());
-        }
-    }
-
-    private Map<ReportType, JpaRepository<?, Long>> getContentDeleters() {
-        return Map.of(
+    public ContentDeletionHandler(ProjectBoardRepository projectBoardRepository,
+                                  StudyBoardRepository studyBoardRepository,
+                                  MarketplaceBoardRepository marketplaceBoardRepository,
+                                  TutoringBoardRepository tutoringBoardRepository) {
+        this.contentDeleters = Map.of(
                 ReportType.PROJECT_BOARD, projectBoardRepository,
                 ReportType.STUDY_BOARD, studyBoardRepository,
                 ReportType.MARKETPLACE_BOARD, marketplaceBoardRepository,
                 ReportType.TUTORING_BOARD, tutoringBoardRepository
         );
+    }
+
+    public void deleteContent(Report report) {
+        JpaRepository<?, Long> repository = contentDeleters.get(report.getReportType());
+
+        if (repository != null) {
+            repository.deleteById(report.getTargetId());
+        }
     }
 }
