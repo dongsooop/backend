53d5e2d57906ac405aa3553a9d81b4a79dcdae98
rdyjun <rdyjun00@gmail.com>
2025-07-17 16:04:06 +0900

    refactor(MemberRepositoryCustomImpl): change String_AGG to transform

--- src/main/java/com/dongsoop/dongsoop/member/dto/LoginMemberDetails.java
+++ src/main/java/com/dongsoop/dongsoop/member/dto/LoginMemberDetails.java
@@ -2,13 +2,10 @@ package com.dongsoop.dongsoop.member.dto;
 
 import com.dongsoop.dongsoop.department.entity.DepartmentType;
 import com.dongsoop.dongsoop.role.entity.RoleType;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.util.StringUtils;
 
 @Getter
 @NoArgsConstructor
@@ -24,23 +21,4 @@ public class LoginMemberDetails {
     private DepartmentType departmentType;
 
     private List<RoleType> role;
-
-    public LoginMemberDetails(Long id, String nickname, String email, DepartmentType departmentType, String role) {
-        this.id = id;
-        this.nickname = nickname;
-        this.email = email;
-        this.departmentType = departmentType;
-        this.role = parseRoles(role);
-    }
-
-    private List<RoleType> parseRoles(String role) {
-        if (!StringUtils.hasText(role)) {
-            return Collections.emptyList();
-        }
-        
-        return Arrays.stream(role.split(","))
-                .map(String::trim)
-                .map(RoleType::valueOf)
-                .toList();
-    }
 }
