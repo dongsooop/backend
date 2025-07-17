084ccddd570aa97697451d1bf8abc88872c3a810
alpin87 <qortmdals94@naver.com>
2025-07-17 15:16:57 +0900

    feat: create Elasticsearch repository for board search

---
+++ src/main/java/com/dongsoop/dongsoop/search/repository/BoardSearchRepository.java
@@ -0,0 +1,15 @@
+package com.dongsoop.dongsoop.search.repository;
+
+import com.dongsoop.dongsoop.search.entity.BoardDocument;
+import org.springframework.data.elasticsearch.annotations.Query;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BoardSearchRepository extends ElasticsearchRepository<BoardDocument, String> {
+
+    @Query("{\"bool\": {\"should\": [{\"match\": {\"title\": \"?0\"}}, {\"match\": {\"content\": \"?0\"}}]}}")
+    List<BoardDocument> findByTitleContainingOrContentContaining(String title, String content);
+}
