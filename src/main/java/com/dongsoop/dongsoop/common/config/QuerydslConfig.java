52998a396c6a07e4b2b51ce16b2fb40775c12a0e
rdyjun <rdyjun00@gmail.com>
2025-07-17 16:37:35 +0900

    Revert "refactor(QuerydslConfig): add JPQLTemplates DEFAULT to JPAQueryFactory dpendency injection method"
    This reverts commit 2b8ee641aa14a98b81598e64345a994d931c7b65.

--- src/main/java/com/dongsoop/dongsoop/common/config/QuerydslConfig.java
+++ src/main/java/com/dongsoop/dongsoop/common/config/QuerydslConfig.java
@@ -1,6 +1,5 @@
 package com.dongsoop.dongsoop.common.config;
 
-import com.querydsl.jpa.JPQLTemplates;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +10,6 @@ public class QuerydslConfig {
 
     @Bean
     public JPAQueryFactory jpaQueryFactory(EntityManager em) {
-        return new JPAQueryFactory(JPQLTemplates.DEFAULT, em);
+        return new JPAQueryFactory(em);
     }
 }
