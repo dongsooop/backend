7eae111e50c2cb4c5c6724155cc949d2e6f06509
alpin87 <qortmdals94@naver.com>
2025-07-18 09:34:23 +0900

    feat(search): add Elasticsearch-level filtering for board type search

--- src/main/java/com/dongsoop/dongsoop/search/repository/BoardSearchRepository.java
+++ src/main/java/com/dongsoop/dongsoop/search/repository/BoardSearchRepository.java
@@ -1,6 +1,8 @@
 package com.dongsoop.dongsoop.search.repository;
 
 import com.dongsoop.dongsoop.search.entity.BoardDocument;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
@@ -12,4 +14,7 @@ public interface BoardSearchRepository extends ElasticsearchRepository<BoardDocu
 
     @Query("{\"bool\": {\"should\": [{\"match\": {\"title\": \"?0\"}}, {\"match\": {\"content\": \"?0\"}}]}}")
     List<BoardDocument> findByTitleContainingOrContentContaining(String title, String content);
+
+    @Query("{\"bool\": {\"must\": [{\"bool\": {\"should\": [{\"match\": {\"title\": \"?0\"}}, {\"match\": {\"content\": \"?0\"}}]}}, {\"term\": {\"board_type\": \"?1\"}}]}}")
+    Page<BoardDocument> findByKeywordAndBoardType(String keyword, String boardType, Pageable pageable);
 }
