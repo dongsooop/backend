a390a55697f6b5280295baee2355c5442e8c9030
alpin87 <qortmdals94@naver.com>
2025-07-19 18:21:03 +0900

    style: Set up a consistent line

--- src/main/java/com/dongsoop/dongsoop/chat/exception/KickedUserInviteException.java
+++ src/main/java/com/dongsoop/dongsoop/chat/exception/KickedUserInviteException.java
@@ -4,7 +4,7 @@ import com.dongsoop.dongsoop.common.exception.CustomException;
 import org.springframework.http.HttpStatus;
 
 public class KickedUserInviteException extends CustomException {
-  public KickedUserInviteException(Long userId) {
-    super("사용자 " + userId + "는 강퇴된 상태로 초대할 수 없습니다.", HttpStatus.BAD_REQUEST);
-  }
+    public KickedUserInviteException(Long userId) {
+        super("사용자 " + userId + "는 강퇴된 상태로 초대할 수 없습니다.", HttpStatus.BAD_REQUEST);
+    }
 }
