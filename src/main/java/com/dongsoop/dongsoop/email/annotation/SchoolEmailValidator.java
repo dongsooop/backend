d45d244651f069546a769854fc5ea27649a18316
rdyjun <rdyjun00@gmail.com>
2025-07-26 01:09:37 +0900

    fix(SchoolEmailValidator): change email regex to pattern

--- src/main/java/com/dongsoop/dongsoop/email/annotation/SchoolEmailValidator.java
+++ src/main/java/com/dongsoop/dongsoop/email/annotation/SchoolEmailValidator.java
@@ -3,10 +3,11 @@ package com.dongsoop.dongsoop.email.annotation;
 import io.micrometer.common.util.StringUtils;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
+import java.util.regex.Pattern;
 
 public class SchoolEmailValidator implements ConstraintValidator<SchoolEmail, String> {
 
-    private static final String EMAIL_REGEX = "^[a-zA-Z0-9]+@dongyang.ac.kr$";
+    private static final Pattern EMAIL_PATTERN = Pattern.compile("^[a-zA-Z0-9]+@dongyang.ac.kr$");
 
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
@@ -14,6 +15,6 @@ public class SchoolEmailValidator implements ConstraintValidator<SchoolEmail, St
             return true;
         }
 
-        return EMAIL_REGEX.matches(value);
+        return EMAIL_PATTERN.matcher(value).matches();
     }
 }
