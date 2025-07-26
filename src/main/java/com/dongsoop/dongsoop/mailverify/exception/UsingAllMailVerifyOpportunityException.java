5d75422ed3b23598e3f8933dce3f0158888801a0
rdyjun <rdyjun00@gmail.com>
2025-07-26 18:05:17 +0900

    style(UsingAllMailVerifyOpportunityException): remove duplicated end dot

--- src/main/java/com/dongsoop/dongsoop/mailverify/exception/UsingAllMailVerifyOpportunityException.java
+++ src/main/java/com/dongsoop/dongsoop/mailverify/exception/UsingAllMailVerifyOpportunityException.java
@@ -6,6 +6,6 @@ import org.springframework.http.HttpStatus;
 public class UsingAllMailVerifyOpportunityException extends CustomException {
 
     public UsingAllMailVerifyOpportunityException() {
-        super("모든 메일 인증 기회를 사용하였습니다. 인증 코드를 새로 발급해주세요..", HttpStatus.BAD_REQUEST);
+        super("모든 메일 인증 기회를 사용하였습니다. 인증 코드를 새로 발급해주세요.", HttpStatus.BAD_REQUEST);
     }
 }
