c2bd39bdfe5a6252d37a0f9f9dfc365a9ba250fd
rdyjun <rdyjun00@gmail.com>
2025-07-26 19:52:35 +0900

    refactor(MailVerifyServiceImpl): add ThreadLocal for SecureRandom

--- src/main/java/com/dongsoop/dongsoop/mailverify/service/MailVerifyServiceImpl.java
+++ src/main/java/com/dongsoop/dongsoop/mailverify/service/MailVerifyServiceImpl.java
@@ -19,7 +19,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class MailVerifyServiceImpl implements MailVerifyService {
 
-    private static final SecureRandom secureRandom = new SecureRandom();
+    private static final ThreadLocal<SecureRandom> secureRandom = ThreadLocal.withInitial(SecureRandom::new);
     private static final String SUBJECT = "[동숲] 인증번호 발송";
     private static final String CHAR_POOL = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     private static final int CODE_LENGTH = 6;
@@ -41,7 +41,6 @@ public class MailVerifyServiceImpl implements MailVerifyService {
         String verifyCode = generateVerificationCode();
 
         String redisKey = getRedisKeyByHashed(to);
-        System.out.println(redisKey);
         redisTemplate.opsForHash()
                 .putAll(redisKey, Map.of(VERIFY_CODE_KEY, verifyCode, OPPORTUNITY_KEY, DEFAULT_OPPORTUNITY_COUNT));
         redisTemplate.expire(redisKey, Duration.ofSeconds(VERIFY_CODE_EXPIRATION_TIME));
@@ -58,7 +57,7 @@ public class MailVerifyServiceImpl implements MailVerifyService {
     private String generateVerificationCode() {
         StringBuilder codeBuilder = new StringBuilder(CODE_LENGTH);
         for (int i = 0; i < CODE_LENGTH; i++) {
-            int index = secureRandom.nextInt(CHAR_POOL.length());
+            int index = secureRandom.get().nextInt(CHAR_POOL.length());
             codeBuilder.append(CHAR_POOL.charAt(index));
         }
         return codeBuilder.toString();
@@ -67,7 +66,6 @@ public class MailVerifyServiceImpl implements MailVerifyService {
     @Override
     public void validateVerificationCode(String email, String code) {
         String redisKey = getRedisKeyByHashed(email);
-        System.out.println(redisKey);
 
         Integer storedOpportunity = getStoredOpportunity(redisKey);
         if (storedOpportunity == null || storedOpportunity <= 0) {
