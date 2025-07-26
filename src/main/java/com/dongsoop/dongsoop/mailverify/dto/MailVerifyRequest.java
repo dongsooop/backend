cec371c08c267b1b0ef5fe1a41ce9b602be6e5c4
rdyjun <rdyjun00@gmail.com>
2025-07-26 19:09:24 +0900

    refactor(MailVerify): add validate NotBlank from code field

--- src/main/java/com/dongsoop/dongsoop/mailverify/dto/MailVerifyRequest.java
+++ src/main/java/com/dongsoop/dongsoop/mailverify/dto/MailVerifyRequest.java
@@ -11,6 +11,7 @@ public record MailVerifyRequest(
         String to,
 
         @Pattern(regexp = "^[A-Z0-9]{6}$", message = "인증 코드는 대문자, 숫자로 이루어진 6자리여야 합니다.")
+        @NotBlank(message = "인증 코드는 필수 입력값입니다.")
         String code
 ) {
 }
