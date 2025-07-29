70bf6497dfd980926422d700b22954a8fe1a3bda
rdyjun <rdyjun00@gmail.com>
2025-07-29 16:07:53 +0900

    refactor(NoticeParsingException): change department to department name

--- src/main/java/com/dongsoop/dongsoop/notice/exception/NoticeParsingException.java
+++ src/main/java/com/dongsoop/dongsoop/notice/exception/NoticeParsingException.java
@@ -5,7 +5,7 @@ import com.dongsoop.dongsoop.department.entity.Department;
 public class NoticeParsingException extends RuntimeException {
 
     public NoticeParsingException(Department department, Long maxId, Exception e) {
-        super("공지사항 파싱 중 오류 발생. department: " + department + ", maxId: " + maxId, e);
+        super("공지사항 파싱 중 오류 발생. department: " + department.getId().name() + ", maxId: " + maxId, e);
     }
 
 }
