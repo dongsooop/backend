ecba6ff5e7de657784f17bd327fe7c54e5cb1cd9
rdyjun <rdyjun00@gmail.com>
2025-07-31 12:15:16 +0900

    refactor(RecruitmentValidationConstant): add initial consonant to tag regex

--- src/main/java/com/dongsoop/dongsoop/recruitment/validation/constant/RecruitmentValidationConstant.java
+++ src/main/java/com/dongsoop/dongsoop/recruitment/validation/constant/RecruitmentValidationConstant.java
@@ -8,7 +8,7 @@ public final class RecruitmentValidationConstant {
 
     public static final int TAG_MAX_LENGTH = 100;
 
-    private static final String AVAILABLE_TAG_TEXT = "[a-zA-Z0-9가-힣]";
+    private static final String AVAILABLE_TAG_TEXT = "[a-zA-Z0-9ㄱ-ㅎ가-힣]";
     private static final String TAG_LENGTH_REGEX = "{1," + TAG_MAX_LENGTH + "}";
     private static final String START_TAG_REGEX = AVAILABLE_TAG_TEXT + TAG_LENGTH_REGEX;
     private static final String NEXT_TAG_PATTERN =
