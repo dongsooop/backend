1152eab5738985d58d2cddb20faca761a563d586
rdyjun <rdyjun00@gmail.com>
2025-08-10 15:24:27 +0900

    refactor(UnregisteredDeviceException): change HttpStatus 500 to 404

--- src/main/java/com/dongsoop/dongsoop/memberdevice/exception/UnregisteredDeviceException.java
+++ src/main/java/com/dongsoop/dongsoop/memberdevice/exception/UnregisteredDeviceException.java
@@ -6,6 +6,6 @@ import org.springframework.http.HttpStatus;
 public class UnregisteredDeviceException extends CustomException {
 
     public UnregisteredDeviceException() {
-        super("등록되지 않은 기기입니다.", HttpStatus.INTERNAL_SERVER_ERROR);
+        super("등록되지 않은 기기입니다.", HttpStatus.NOT_FOUND);
     }
 }
