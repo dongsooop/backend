b951767f8c7b40092e735848722b8e093dbd16a5
rdyjun <rdyjun00@gmail.com>
2025-08-10 15:39:12 +0900

    refactor(AlreadyRegisteredDeviceException): add HttpStatus CONFLICT

--- src/main/java/com/dongsoop/dongsoop/memberdevice/exception/AlreadyRegisteredDeviceException.java
+++ src/main/java/com/dongsoop/dongsoop/memberdevice/exception/AlreadyRegisteredDeviceException.java
@@ -1,8 +1,11 @@
 package com.dongsoop.dongsoop.memberdevice.exception;
 
-public class AlreadyRegisteredDeviceException extends RuntimeException {
+import com.dongsoop.dongsoop.common.exception.CustomException;
+import org.springframework.http.HttpStatus;
+
+public class AlreadyRegisteredDeviceException extends CustomException {
 
     public AlreadyRegisteredDeviceException() {
-        super("이미 등록된 디바이스입니다.");
+        super("이미 등록된 디바이스입니다.", HttpStatus.CONFLICT);
     }
 }
