81bdb1e0fb8280950164857e40b553f5f70c80c6
alpin87 <qortmdals94@naver.com>
2025-08-11 09:34:10 +0900

    feat: Implement real-time block status notifications via WebSocket

--- src/main/java/com/dongsoop/dongsoop/memberblock/service/MemberBlockServiceImpl.java
+++ src/main/java/com/dongsoop/dongsoop/memberblock/service/MemberBlockServiceImpl.java
@@ -1,5 +1,10 @@
 package com.dongsoop.dongsoop.memberblock.service;
 
+import com.dongsoop.dongsoop.chat.entity.BlockStatus;
+import com.dongsoop.dongsoop.chat.entity.ChatRoom;
+import com.dongsoop.dongsoop.chat.exception.ChatRoomNotFoundException;
+import com.dongsoop.dongsoop.chat.repository.RedisChatRepository;
+import com.dongsoop.dongsoop.chat.service.ChatService;
 import com.dongsoop.dongsoop.member.entity.Member;
 import com.dongsoop.dongsoop.member.repository.MemberRepository;
 import com.dongsoop.dongsoop.member.service.MemberService;
@@ -23,12 +28,14 @@ public class MemberBlockServiceImpl implements MemberBlockService {
     private final MemberRepository memberRepository;
     private final MemberBlockRepository memberBlockRepository;
     private final MemberBlockRepositoryCustom memberBlockRepositoryCustom;
+    private final ChatService chatService;
+    private final RedisChatRepository redisChatRepository;
 
     @Override
     public void blockMember(MemberBlockRequest request) {
         Member blocker = memberRepository.getReferenceById(request.blockerId());
-        Member blockedMemberId = memberRepository.getReferenceById(request.blockedMemberId());
-        MemberBlockId memberBlockId = new MemberBlockId(blocker, blockedMemberId);
+        Member blockedMember = memberRepository.getReferenceById(request.blockedMemberId());
+        MemberBlockId memberBlockId = new MemberBlockId(blocker, blockedMember);
 
         if (memberBlockRepository.existsById(memberBlockId)) {
             throw new AlreadyBlockedByBlockerException();
@@ -36,13 +43,16 @@ public class MemberBlockServiceImpl implements MemberBlockService {
 
         MemberBlock memberBlock = new MemberBlock(memberBlockId);
         memberBlockRepository.save(memberBlock);
+
+        sendBlockNotification(blocker, blockedMember, BlockStatus.I_BLOCKED);
     }
+
 
     @Override
     public void unblockMember(MemberBlockRequest request) {
         Member blocker = memberRepository.getReferenceById(request.blockerId());
-        Member blockedMemberId = memberRepository.getReferenceById(request.blockedMemberId());
-        MemberBlockId memberBlockId = new MemberBlockId(blocker, blockedMemberId);
+        Member blockedMember = memberRepository.getReferenceById(request.blockedMemberId());
+        MemberBlockId memberBlockId = new MemberBlockId(blocker, blockedMember);
 
         if (!memberBlockRepository.existsById(memberBlockId)) {
             throw new BlockNotFoundException();
@@ -50,11 +60,22 @@ public class MemberBlockServiceImpl implements MemberBlockService {
 
         MemberBlock memberBlock = memberBlockRepository.getReferenceById(memberBlockId);
         memberBlockRepository.delete(memberBlock);
+
+        sendBlockNotification(blocker, blockedMember, BlockStatus.NONE);
     }
 
     @Override
     public List<BlockedMember> getBlockedMember() {
         Long requesterId = memberService.getMemberIdByAuthentication();
         return memberBlockRepositoryCustom.findByBlockerId(requesterId);
+    }
+
+    private void sendBlockNotification(Member blocker, Member blockedMember, BlockStatus blockStatus) {
+        ChatRoom room = redisChatRepository.findRoomByParticipants(blocker.getId(), blockedMember.getId())
+                .orElseThrow(ChatRoomNotFoundException::new);
+        String roomId = room.getRoomId();
+
+        chatService.sendBlockStatusToUser(roomId, blocker.getId(), blockStatus);
+        chatService.sendBlockStatusToUser(roomId, blockedMember.getId(), BlockStatus.BLOCKED_BY_OTHER);
     }
 }
