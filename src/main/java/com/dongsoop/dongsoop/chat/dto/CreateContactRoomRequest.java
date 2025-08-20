0db39cb4da63fa52dc3ff69f02ffc7bc2e11aa20
alpin87 <qortmdals94@naver.com>
2025-08-20 10:19:33 +0900

    feat: Create a parameter dto for creating an inquiry room

---
+++ src/main/java/com/dongsoop/dongsoop/chat/dto/CreateContactRoomRequest.java
@@ -0,0 +1,15 @@
+package com.dongsoop.dongsoop.chat.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateContactRoomRequest {
+    private Long targetUserId;
+    private String boardType;
+    private Long boardId;
+    private String boardTitle;
+}
