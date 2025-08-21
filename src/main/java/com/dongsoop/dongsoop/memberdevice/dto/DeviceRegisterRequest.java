462980bc83e55293a2aebaeb1d6ac74dac7d5eeb
rdyjun <rdyjun00@gmail.com>
2025-08-21 10:53:12 +0900

    refactor(DeviceRegisterRequest): add validate annotation

--- src/main/java/com/dongsoop/dongsoop/memberdevice/dto/DeviceRegisterRequest.java
+++ src/main/java/com/dongsoop/dongsoop/memberdevice/dto/DeviceRegisterRequest.java
@@ -1,10 +1,15 @@
 package com.dongsoop.dongsoop.memberdevice.dto;
 
 import com.dongsoop.dongsoop.memberdevice.entity.MemberDeviceType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record DeviceRegisterRequest(
 
+        @NotBlank
         String deviceToken,
+
+        @NotNull
         MemberDeviceType type
 ) {
 }
