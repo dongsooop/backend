7768db89f591f08834df85680b40fd5b9c80c1fc
rdyjun <rdyjun00@gmail.com>
2025-08-28 12:27:12 +0900

    refactor(ChatNotificationImpl): separate notification save method to notification service

--- src/main/java/com/dongsoop/dongsoop/chat/notification/ChatNotificationImpl.java
+++ src/main/java/com/dongsoop/dongsoop/chat/notification/ChatNotificationImpl.java
@@ -3,10 +3,8 @@ package com.dongsoop.dongsoop.chat.notification;
 import com.dongsoop.dongsoop.memberdevice.dto.MemberDeviceDto;
 import com.dongsoop.dongsoop.memberdevice.repository.MemberDeviceRepositoryCustom;
 import com.dongsoop.dongsoop.notification.constant.NotificationType;
-import com.dongsoop.dongsoop.notification.entity.MemberNotification;
-import com.dongsoop.dongsoop.notification.entity.NotificationDetails;
-import com.dongsoop.dongsoop.notification.repository.NotificationRepository;
 import com.dongsoop.dongsoop.notification.service.FCMService;
+import com.dongsoop.dongsoop.notification.service.NotificationService;
 import java.util.List;
 import java.util.Set;
 import lombok.RequiredArgsConstructor;
@@ -18,7 +16,7 @@ public class ChatNotificationImpl implements ChatNotification {
 
     private final MemberDeviceRepositoryCustom memberDeviceRepositoryCustom;
     private final FCMService fcmService;
-    private final NotificationRepository notificationRepository;
+    private final NotificationService notificationService;
 
     public void send(Set<Long> chatroomMemberIdSet, String chatRoomId, String senderName,
                      String message) {
@@ -32,23 +30,6 @@ public class ChatNotificationImpl implements ChatNotification {
 
         fcmService.sendNotification(deviceTokens, senderName, message, NotificationType.CHAT, chatRoomId);
 
-        saveMemberNotification(chatRoomId, senderName, message, participantsDevice);
-    }
-
-    private void saveMemberNotification(String chatRoomId, String senderName,
-                                        String message,
-                                        List<MemberDeviceDto> participantsDevice) {
-        NotificationDetails details = NotificationDetails.builder()
-                .title(senderName)
-                .body(message)
-                .type(NotificationType.CHAT)
-                .value(chatRoomId)
-                .build();
-
-        List<MemberNotification> memberNotificationList = participantsDevice.stream()
-                .map(memberDeviceDto -> new MemberNotification(details, memberDeviceDto.member()))
-                .toList();
-
-        notificationRepository.saveAll(memberNotificationList);
+        notificationService.save(participantsDevice, senderName, message, NotificationType.CHAT, chatRoomId);
     }
 }
