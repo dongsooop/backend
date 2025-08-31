0a49548aaa95337692f52e768df285fb5952a631
rdyjun <rdyjun00@gmail.com>
2025-08-31 14:42:59 +0900

    refactor(ResponseSizeUnmatchedToTokenSizeException): change String to String format

--- src/main/java/com/dongsoop/dongsoop/notification/exception/ResponseSizeUnmatchedToTokenSizeException.java
+++ src/main/java/com/dongsoop/dongsoop/notification/exception/ResponseSizeUnmatchedToTokenSizeException.java
@@ -6,7 +6,7 @@ import org.springframework.http.HttpStatus;
 public class ResponseSizeUnmatchedToTokenSizeException extends CustomException {
 
     public ResponseSizeUnmatchedToTokenSizeException(int responseSize, int tokenSize) {
-        super("알림 응답의 크기가 토큰 목록의 크기와 일치하지 않습니다.\n응답 수: " + responseSize + " 토큰 수: " + tokenSize,
+        super(String.format("알림 응답의 크기가 토큰 목록의 크기와 일치하지 않습니다.%n응답 수: %d, 토큰 수: %d", responseSize, tokenSize),
                 HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
