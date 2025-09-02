c55fab65aef16c7eddad48031b8c220057d082ca
rdyjun <rdyjun00@gmail.com>
2025-09-03 01:58:43 +0900

    feat(fcm-service): subscribe to topic when register device

--- src/main/java/com/dongsoop/dongsoop/memberdevice/controller/MemberDeviceController.java
+++ src/main/java/com/dongsoop/dongsoop/memberdevice/controller/MemberDeviceController.java
@@ -2,8 +2,11 @@ package com.dongsoop.dongsoop.memberdevice.controller;
 
 import com.dongsoop.dongsoop.memberdevice.dto.DeviceRegisterRequest;
 import com.dongsoop.dongsoop.memberdevice.service.MemberDeviceService;
+import com.dongsoop.dongsoop.notification.service.FCMService;
 import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,10 +19,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberDeviceController {
 
     private final MemberDeviceService memberDeviceService;
+    private final FCMService fcmService;
+
+    @Value("${notification.topic.anonymous}")
+    private String anonymousTopic;
 
     @PostMapping
     public ResponseEntity<Void> registerDevice(@RequestBody @Valid DeviceRegisterRequest request) {
         memberDeviceService.registerDevice(request.deviceToken(), request.type());
+        fcmService.subscribeTopic(List.of(anonymousTopic), anonymousTopic);
 
         return ResponseEntity.noContent()
                 .build();
