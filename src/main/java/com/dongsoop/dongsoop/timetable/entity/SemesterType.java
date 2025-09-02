73e145305783b657ce8b5b09de0d5ca2dae1a33b
rdyjun <rdyjun00@gmail.com>
2025-09-02 14:57:16 +0900

    refactor(SemesterType): separate number variable

--- src/main/java/com/dongsoop/dongsoop/timetable/entity/SemesterType.java
+++ src/main/java/com/dongsoop/dongsoop/timetable/entity/SemesterType.java
@@ -9,20 +9,27 @@ public enum SemesterType {
     SUMMER,
     WINTER;
 
+    private static final int WINTER_MONTH = 1;
+    private static final int SUMMER_MONTH = 7;
+    private static final int FIRST_SEMESTER_START_MONTH = 3;
+    private static final int FIRST_SEMESTER_END_MONTH = 6;
+    private static final int SECOND_SEMESTER_START_MONTH = 9;
+    private static final int SECOND_SEMESTER_END_MONTH = 12;
+
     public static SemesterType fromMonth(int month) {
-        if (month == 1) {
+        if (month == WINTER_MONTH) {
             return WINTER;
         }
 
-        if (month == 7) {
+        if (month == SUMMER_MONTH) {
             return SUMMER;
         }
 
-        if (month >= 3 && month <= 6) {
+        if (month >= FIRST_SEMESTER_START_MONTH && month <= FIRST_SEMESTER_END_MONTH) {
             return FIRST;
         }
 
-        if (month >= 9 && month <= 12) {
+        if (month >= SECOND_SEMESTER_START_MONTH && month <= SECOND_SEMESTER_END_MONTH) {
             return SECOND;
         }
 
