a4b8bd9f6152b348d78e05d19ce80f38a78429d5
rdyjun <rdyjun00@gmail.com>
2025-09-08 17:42:35 +0900

    feat(health-check): add health check api

---
+++ src/main/java/com/dongsoop/dongsoop/common/healthcheck/HealthCheck.java
@@ -0,0 +1,17 @@
+package com.dongsoop.dongsoop.common.healthcheck;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthCheck {
+
+    @GetMapping
+    public ResponseEntity<Void> healthCheck() {
+        return ResponseEntity.ok()
+                .build();
+    }
+}
