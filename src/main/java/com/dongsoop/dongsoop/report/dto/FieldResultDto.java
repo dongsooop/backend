aaef5e5e016f85fbbe8ca030921ca4df2ac8e5ce
alpin87 <qortmdals94@naver.com>
2025-09-15 10:20:42 +0900

    refactor: rename method from isHasProfanity to hasProfanity

--- src/main/java/com/dongsoop/dongsoop/report/dto/FieldResultDto.java
+++ src/main/java/com/dongsoop/dongsoop/report/dto/FieldResultDto.java
@@ -3,7 +3,7 @@ package com.dongsoop.dongsoop.report.dto;
 public class FieldResultDto {
     private boolean has_profanity;
 
-    public boolean isHasProfanity() {
+    public boolean hasProfanity() {
         return has_profanity;
     }
 }
