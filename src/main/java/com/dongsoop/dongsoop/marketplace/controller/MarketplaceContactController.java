3efdc2605aa086e4bf8a5957a8494ecc47531f51
alpin87 <qortmdals94@naver.com>
2025-09-23 10:04:27 +0900

    refactor(marketplace): Modify contact API to respond with roomId

--- src/main/java/com/dongsoop/dongsoop/marketplace/controller/MarketplaceContactController.java
+++ src/main/java/com/dongsoop/dongsoop/marketplace/controller/MarketplaceContactController.java
@@ -3,7 +3,6 @@ package com.dongsoop.dongsoop.marketplace.controller;
 import com.dongsoop.dongsoop.marketplace.dto.ContactMarketplaceRequest;
 import com.dongsoop.dongsoop.marketplace.service.MarketplaceContactService;
 import com.dongsoop.dongsoop.role.entity.RoleType;
-import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
@@ -11,6 +10,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/marketplace-contact")
@@ -21,12 +23,13 @@ public class MarketplaceContactController {
 
     @PostMapping
     @Secured(RoleType.USER_ROLE)
-    public ResponseEntity<Void> contactMarketplace(@RequestBody ContactMarketplaceRequest request) {
-        marketplaceContactService.contact(request);
-
+    public ResponseEntity<Map<String, String>> contactMarketplace(@RequestBody ContactMarketplaceRequest request) {
+        String roomId = marketplaceContactService.contact(request);
         URI uri = URI.create("/marketplace-board/" + request.boardId());
 
+        Map<String, String> responseBody = Map.of("roomId", roomId);
+
         return ResponseEntity.created(uri)
-                .build();
+                .body(responseBody);
     }
 }
