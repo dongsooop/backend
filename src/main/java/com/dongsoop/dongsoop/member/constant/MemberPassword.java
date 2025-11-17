a19a53bb21bc06826b4766215337c7d734f919b0
rdyjun <rdyjun00@gmail.com>
2025-11-17 16:25:37 +0900

    add comment for password

--- src/main/java/com/dongsoop/dongsoop/member/constant/MemberPassword.java
+++ src/main/java/com/dongsoop/dongsoop/member/constant/MemberPassword.java
@@ -2,5 +2,6 @@ package com.dongsoop.dongsoop.member.constant;
 
 public class MemberPassword {
 
-    public static final String regex = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*()_\\-+={}\\[\\]?,./])[A-Za-z\\d!@#$%^&*()_\\-+={}\\[\\]?,./]{8,20}$";
+    // 8글자 이상 20글자 이하, 영문, 숫자, 특수문자 포함
+    public static final String regex = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*()_\\-+={}\\[\\]?,./~])[A-Za-z\\d!@#$%^&*()_\\-+={}\\[\\]?,./~]{8,20}$";
 }
