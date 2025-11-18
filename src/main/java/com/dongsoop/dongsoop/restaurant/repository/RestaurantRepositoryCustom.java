ab891a6d2936cbf414f248d045d3847b610a2158
alpin87 <qortmdals94@naver.com>
2025-11-18 11:11:17 +0900

    feat(restaurant): Define custom query interface for restaurant search

---
+++ src/main/java/com/dongsoop/dongsoop/restaurant/repository/RestaurantRepositoryCustom.java
@@ -0,0 +1,15 @@
+package com.dongsoop.dongsoop.restaurant.repository;
+
+import com.dongsoop.dongsoop.restaurant.dto.RestaurantOverview;
+import com.dongsoop.dongsoop.restaurant.entity.RestaurantStatus;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface RestaurantRepositoryCustom {
+    List<RestaurantOverview> findNearbyRestaurants(
+            double latitude, double longitude, double distanceKm, Long memberId, Pageable pageable);
+
+    List<RestaurantOverview> findRestaurantsByStatus(
+            RestaurantStatus status, Pageable pageable, Long memberId);
+}
