11ce286ca7d6c412b7adeadd1712287828d1bbc0
alpin87 <qortmdals94@naver.com>
2025-11-18 11:01:15 +0900

    feat(restaurant): Define DTO for restaurant list overview

---
+++ src/main/java/com/dongsoop/dongsoop/restaurant/dto/RestaurantOverview.java
@@ -0,0 +1,48 @@
+package com.dongsoop.dongsoop.restaurant.dto;
+
+import com.dongsoop.dongsoop.restaurant.entity.RestaurantCategory;
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.Getter;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+@Getter
+public final class RestaurantOverview {
+    private final Long id;
+    private final String name;
+    private final String phone;
+    private final String category;
+    private final Double latitude;
+    private final Double longitude;
+
+    private final long likeCount;
+    private final boolean isLikedByMe;
+    private final List<String> tags;
+    private final String externalMapId;
+
+    @QueryProjection
+    public RestaurantOverview(Long id, String name, String phone, RestaurantCategory categoryEnum,
+                              Double latitude, Double longitude,
+                              long likeCount, boolean isLikedByMe, String tags,
+                              String externalMapId) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+        this.category = categoryEnum.getDisplayName();
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.likeCount = likeCount;
+        this.isLikedByMe = isLikedByMe;
+        this.tags = parseTags(tags);
+        this.externalMapId = externalMapId;
+    }
+
+    private static List<String> parseTags(String tags) {
+        if (tags == null || tags.isEmpty()) {
+            return Collections.emptyList();
+        }
+        return Arrays.asList(tags.split(","));
+    }
+}
