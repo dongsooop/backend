48a973209edba200d8fe8db99e493ae8e184136d
alpin87 <qortmdals94@naver.com>
2025-11-20 10:26:35 +0900

    fix(RestaurantLike): exclude associations from @ToString to prevent circular references

--- src/main/java/com/dongsoop/dongsoop/restaurant/entity/RestaurantLike.java
+++ src/main/java/com/dongsoop/dongsoop/restaurant/entity/RestaurantLike.java
@@ -2,17 +2,15 @@ package com.dongsoop.dongsoop.restaurant.entity;
 
 import com.dongsoop.dongsoop.member.entity.Member;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import lombok.experimental.SuperBuilder;
 
 import java.io.Serializable;
-import java.util.Objects;
 
 @Entity
 @SuperBuilder
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(exclude = "id")
 public class RestaurantLike {
 
     @EmbeddedId
@@ -22,6 +20,9 @@ public class RestaurantLike {
     @NoArgsConstructor
     @AllArgsConstructor
     @Builder
+    @Getter
+    @EqualsAndHashCode
+    @ToString(exclude = {"restaurant", "member"})
     public static class RestaurantLikeKey implements Serializable {
 
         @ManyToOne(fetch = FetchType.LAZY)
@@ -31,19 +32,5 @@ public class RestaurantLike {
         @ManyToOne(fetch = FetchType.LAZY)
         @JoinColumn(nullable = false, name = "member_id", updatable = false)
         private Member member;
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            RestaurantLikeKey that = (RestaurantLikeKey) o;
-            return restaurant.equalsId(that.restaurant) &&
-                    Objects.equals(member.getId(), that.member.getId());
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(restaurant.getId(), member.getId());
-        }
     }
 }
