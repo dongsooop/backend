9fa755b864624d7e88a6157732144074c4a6280e
alpin87 <qortmdals94@naver.com>
2025-11-24 14:33:55 +0900

    feat(Restaurant): Adding Multiple Cases to Enumeration File

--- src/main/java/com/dongsoop/dongsoop/restaurant/entity/RestaurantTag.java
+++ src/main/java/com/dongsoop/dongsoop/restaurant/entity/RestaurantTag.java
@@ -9,9 +9,11 @@ public enum RestaurantTag {
     LARGE_PORTION("양이 많아요", "LARGE_PORTION"),
     DELICIOUS("음식이 맛있어요", "DELICIOUS"),
     GOOD_FOR_LUNCH("점심으로 괜찮아요", "GOOD_FOR_LUNCH"),
+    GOOD_FOR_SOLO("혼밥하기 좋아요", "GOOD_FOR_SOLO"),
     GOOD_VALUE("가성비가 좋아요", "GOOD_VALUE"),
     GOOD_FOR_GATHERING("회식하기 좋아요", "GOOD_FOR_GATHERING"),
-    GOOD_FOR_CONVERSATION("대화하기 좋아요", "GOOD_FOR_CONVERSATION");
+    GOOD_FOR_CONVERSATION("대화하기 괜찮아요", "GOOD_FOR_CONVERSATION"),
+    VARIOUS_MENU("메뉴가 다양해요", "VARIOUS_MENU");
 
     private final String description;
     private final String code;
