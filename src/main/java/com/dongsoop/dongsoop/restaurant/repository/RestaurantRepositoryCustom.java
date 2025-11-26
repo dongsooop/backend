6718cd2f5e9d2bb0d81e193442881629792ad4db
alpin87 <qortmdals94@naver.com>
2025-11-26 09:54:48 +0900

    feat: Implement dynamic category filtering and add placeUrl to projection

--- src/main/java/com/dongsoop/dongsoop/restaurant/repository/RestaurantRepositoryCustom.java
+++ src/main/java/com/dongsoop/dongsoop/restaurant/repository/RestaurantRepositoryCustom.java
@@ -1,12 +1,13 @@
 package com.dongsoop.dongsoop.restaurant.repository;
 
 import com.dongsoop.dongsoop.restaurant.dto.RestaurantOverview;
+import com.dongsoop.dongsoop.restaurant.entity.RestaurantCategory;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface RestaurantRepositoryCustom {
-    List<RestaurantOverview> findNearbyRestaurants(Long memberId, Pageable pageable);
+    List<RestaurantOverview> findNearbyRestaurants(Long memberId, RestaurantCategory category, Pageable pageable);
 
     boolean existsActiveByExternalMapId(String externalMapId);
 }
