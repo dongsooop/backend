5d26d492757eb51da14041b288574524f891984f
rdyjun <rdyjun00@gmail.com>
2026-01-07 11:35:33 +0900

    chore(NotificationSettingRepositoryCustom): remove update method for using dirty checking

--- src/main/java/com/dongsoop/dongsoop/notification/setting/repository/NotificationSettingRepositoryCustom.java
+++ src/main/java/com/dongsoop/dongsoop/notification/setting/repository/NotificationSettingRepositoryCustom.java
@@ -1,13 +1,9 @@
 package com.dongsoop.dongsoop.notification.setting.repository;
 
-import com.dongsoop.dongsoop.memberdevice.entity.MemberDevice;
-import com.dongsoop.dongsoop.notification.constant.NotificationType;
 import com.dongsoop.dongsoop.notification.setting.dto.NotificationActiveStatus;
 import java.util.List;
 
 public interface NotificationSettingRepositoryCustom {
-
-    long updateNotificationSetting(MemberDevice memberDevice, NotificationType type, boolean isEnabled);
 
     List<NotificationActiveStatus> findByDeviceToken(String deviceToken);
 }
--- src/main/java/com/dongsoop/dongsoop/notification/setting/repository/NotificationSettingRepositoryCustomImpl.java
+++ src/main/java/com/dongsoop/dongsoop/notification/setting/repository/NotificationSettingRepositoryCustomImpl.java
@@ -1,7 +1,5 @@
 package com.dongsoop.dongsoop.notification.setting.repository;
 
-import com.dongsoop.dongsoop.memberdevice.entity.MemberDevice;
-import com.dongsoop.dongsoop.notification.constant.NotificationType;
 import com.dongsoop.dongsoop.notification.setting.dto.NotificationActiveStatus;
 import com.dongsoop.dongsoop.notification.setting.entity.QNotificationSetting;
 import com.querydsl.core.types.Projections;
@@ -17,24 +15,6 @@ public class NotificationSettingRepositoryCustomImpl implements NotificationSett
     private static final QNotificationSetting notificationSetting = QNotificationSetting.notificationSetting;
 
     private final JPAQueryFactory queryFactory;
-
-    /**
-     * 알림 설정 업데이트
-     *
-     * @param memberDevice 회원 디바이스
-     * @param type         설정할 알림 타입
-     * @param isEnabled    활성화 여부
-     * @return 업데이트된 행의 수
-     */
-    @Override
-    public long updateNotificationSetting(MemberDevice memberDevice, NotificationType type, boolean isEnabled) {
-        return queryFactory.update(notificationSetting)
-                .set(notificationSetting.enabled, isEnabled)
-                .where(notificationSetting.id.device.eq(memberDevice)
-                        .and(notificationSetting.id.notificationType.eq(type))
-                        .and(notificationSetting.enabled.eq(!isEnabled)))
-                .execute();
-    }
 
     @Override
     public List<NotificationActiveStatus> findByDeviceToken(String deviceToken) {
