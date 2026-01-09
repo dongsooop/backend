9a2432cbd9e2e28cb5ae616e42131f9e3de7be8e
rdyjun <rdyjun00@gmail.com>
2026-01-09 11:14:29 +0900

    chore(NotificationSettingChangeHandler): remove Component annotation to interface

--- src/main/java/com/dongsoop/dongsoop/notification/setting/service/handler/NotificationSettingChangeHandler.java
+++ src/main/java/com/dongsoop/dongsoop/notification/setting/service/handler/NotificationSettingChangeHandler.java
@@ -2,9 +2,7 @@ package com.dongsoop.dongsoop.notification.setting.service.handler;
 
 import com.dongsoop.dongsoop.notification.setting.dto.SettingChanges;
 import java.util.List;
-import org.springframework.stereotype.Component;
 
-@Component
 public interface NotificationSettingChangeHandler {
 
     Class<? extends SettingChanges> getSupportedClass();
