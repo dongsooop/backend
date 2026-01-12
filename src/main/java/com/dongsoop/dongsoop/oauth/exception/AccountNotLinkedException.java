54f0f748d71c5b63c4b9fa89274f9f28534708f6
rdyjun <rdyjun00@gmail.com>
2026-01-12 22:37:41 +0900

    feat(AccountNotLinkedException): change http status 401 to 400

--- src/main/java/com/dongsoop/dongsoop/oauth/exception/AccountNotLinkedException.java
+++ src/main/java/com/dongsoop/dongsoop/oauth/exception/AccountNotLinkedException.java
@@ -1,10 +1,11 @@
 package com.dongsoop.dongsoop.oauth.exception;
 
-import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import com.dongsoop.dongsoop.common.exception.CustomException;
+import org.springframework.http.HttpStatus;
 
-public class AccountNotLinkedException extends OAuth2AuthenticationException {
+public class AccountNotLinkedException extends CustomException {
 
     public AccountNotLinkedException() {
-        super("소셜 계정이 연결되어 있지 않습니다: ");
+        super("소셜 계정이 연결되어 있지 않습니다.", HttpStatus.BAD_REQUEST);
     }
 }
