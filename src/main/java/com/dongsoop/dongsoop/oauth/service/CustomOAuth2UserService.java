100ed8d59d20392db7c15b9087d27b8485334d55
rdyjun <rdyjun00@gmail.com>
2026-01-13 13:13:02 +0900

    fix(CustomOAuth2UserService): Send provider type to validate method

--- src/main/java/com/dongsoop/dongsoop/oauth/service/CustomOAuth2UserService.java
+++ src/main/java/com/dongsoop/dongsoop/oauth/service/CustomOAuth2UserService.java
@@ -2,10 +2,9 @@ package com.dongsoop.dongsoop.oauth.service;
 
 import com.dongsoop.dongsoop.oauth.dto.CustomOAuth2User;
 import com.dongsoop.dongsoop.oauth.dto.MemberSocialAccountDto;
+import com.dongsoop.dongsoop.oauth.entity.OAuthProviderType;
 import com.dongsoop.dongsoop.oauth.provider.OAuth2UserParser;
-import com.dongsoop.dongsoop.oauth.repository.MemberSocialAccountRepository;
 import com.dongsoop.dongsoop.oauth.validator.MemberSocialAccountValidator;
-import com.dongsoop.dongsoop.report.validator.ReportValidator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
@@ -19,9 +18,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
-    private final MemberSocialAccountRepository memberSocialAccountRepository;
     private final MemberSocialAccountValidator memberSocialAccountValidator;
-    private final ReportValidator reportValidator;
     private final OAuth2UserParser oAuth2UserParser;
 
     @Override
@@ -34,8 +31,15 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
                 .getRegistrationId();
         String providerId = this.oAuth2UserParser.extractProviderId(oAuth2User, registrationId);
 
-        MemberSocialAccountDto socialAccount = memberSocialAccountValidator.validate(providerId);
+        try {
+            OAuthProviderType oAuthProviderType = OAuthProviderType.valueOf(registrationId.toUpperCase());
 
-        return new CustomOAuth2User(oAuth2User, socialAccount.member().getId(), socialAccount.roleType());
+            MemberSocialAccountDto socialAccount = memberSocialAccountValidator.validate(providerId, oAuthProviderType);
+
+            return new CustomOAuth2User(oAuth2User, socialAccount.member().getId(), socialAccount.roleType());
+        } catch (IllegalArgumentException e) {
+            log.error("Unsupported OAuth provider: {}", registrationId);
+            throw new OAuth2AuthenticationException("Unsupported OAuth provider: " + registrationId);
+        }
     }
 }
