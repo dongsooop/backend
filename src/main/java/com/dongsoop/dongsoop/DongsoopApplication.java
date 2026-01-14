b366bc6c6a90764dc8a0ab0d7f9dfa8c7ff05a0d
rdyjun <rdyjun00@gmail.com>
2026-01-15 01:23:39 +0900

    feat(DongsoopApplication): Enable caching

--- src/main/java/com/dongsoop/dongsoop/DongsoopApplication.java
+++ src/main/java/com/dongsoop/dongsoop/DongsoopApplication.java
@@ -1,12 +1,14 @@
 package com.dongsoop.dongsoop;
 
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
-
+@EnableCaching
 @SpringBootApplication
 @EnableScheduling
 @EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
