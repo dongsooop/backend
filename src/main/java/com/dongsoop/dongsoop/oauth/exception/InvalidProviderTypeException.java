a98b366ac6bcc883f70278a5742eb9b8e12af538
rdyjun <rdyjun00@gmail.com>
2026-01-15 10:49:29 +0900

    feat(OAuth2Controller): Change providerType's type enum to String

--- src/main/java/com/dongsoop/dongsoop/oauth/exception/InvalidProviderTypeException.java
+++ src/main/java/com/dongsoop/dongsoop/oauth/exception/InvalidProviderTypeException.java
@@ -8,4 +8,8 @@ public class InvalidProviderTypeException extends CustomException {
     public InvalidProviderTypeException() {
         super("유효하지 않은 공급자 타입입니다.", HttpStatus.BAD_REQUEST);
     }
+
+    public InvalidProviderTypeException(String providerType) {
+        super("유효하지 않은 공급자 타입입니다: " + providerType, HttpStatus.BAD_REQUEST);
+    }
 }
