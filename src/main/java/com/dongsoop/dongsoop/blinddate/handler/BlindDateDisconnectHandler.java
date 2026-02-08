4f39a29658fb9b528363cccb83dbc23d91781a2e
rdyjun <rdyjun00@gmail.com>
2026-02-08 16:57:58 +0900

    refactor(BlindDateDisConnectHandler): Add try-catch for safty unlock at remove socket

--- src/main/java/com/dongsoop/dongsoop/blinddate/handler/BlindDateDisconnectHandler.java
+++ src/main/java/com/dongsoop/dongsoop/blinddate/handler/BlindDateDisconnectHandler.java
@@ -38,14 +38,16 @@ public class BlindDateDisconnectHandler {
         // 소켓 제거 중 회원 소켓이 추가되지 않도록 처리
         memberLock.lockByMemberId(memberId);
 
-        // 소켓만 제거 (모든 소켓이 제거되면 자동으로 참여자도 제거됨)
-        boolean fullyRemoved = participantInfoRepository.removeSocket(socketId);
+        try {
+            // 소켓만 제거 (모든 소켓이 제거되면 자동으로 참여자도 제거됨)
+            boolean fullyRemoved = participantInfoRepository.removeSocket(socketId);
 
-        // 소켓 제거 후 락 해제
-        memberLock.unlockByMemberId(memberId);
-
-        // 전체를 삭제한 경우 true 반환
-        return fullyRemoved;
+            // 전체를 삭제한 경우 true 반환
+            return fullyRemoved;
+        } finally {
+            // 락 해제
+            memberLock.unlockByMemberId(memberId);
+        }
     }
 
     private void tryRemoveMember(Long memberId, String sessionId) {
