e58a19d5065aa2f2244defa5fc8c404349721626
rdyjun <rdyjun00@gmail.com>
2026-02-25 16:01:40 +0900

    fix: Enable LocalFCMServiceImpl in test profile

--- src/main/java/com/dongsoop/dongsoop/notification/service/LocalFCMServiceImpl.java
+++ src/main/java/com/dongsoop/dongsoop/notification/service/LocalFCMServiceImpl.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Service;
 @Service
 @Slf4j
 @RequiredArgsConstructor
-@Profile("local")
+@Profile({"local", "test"})
 public class LocalFCMServiceImpl implements FCMService {
     
     @Override
